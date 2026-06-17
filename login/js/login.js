cd6b077abd7c4fba668e3351390a15b740643361
kawaishanio-design <kawaishanio@gmail.com>
2026-06-17 19:36:29 +0700

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -1,12 +1,18 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault(); 
+    
     const usernameInput = document.getElementById('username').value;
     const passwordInput = document.getElementById('password').value;
-    if (usernameInput === 'heri' && passwordInput === '123') {
-        alert('Login Berhasil!');
+    if (passwordInput === '123' && usernameInput + '!') {
+        alert('Login Berhasil, Halo' + usernameInput + '!');
+
+        localStorage.setItem("isLoggedIn", "true");
+        localStorage.setItem("loggedInUser", usernameInput);
+        
         window.location.href = "https://kawaishanio-design.github.io/saya/";
 
     } else {
         alert('Nama Pengguna atau Kata Sandi salah. Silakan coba lagi.');
     }
 });
+ 
